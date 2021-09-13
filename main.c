ff44366c074bcb46b3f4ccfa4c2198d144532493
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-09-13 17:56:51 -0400

    Add: Rusty main file

--- main.c
+++ main.c
@@ -10,10 +10,40 @@
 
 int main(int argc, char **argv)
 {
-	int i;
+	char *path;
+	FILE *funcPointer;
+	unsigned int lineNum = 1;
 
 	/*If the user does not give any file or more than one argument to your
 	/* program, print the error message*/
-	if (argc > 2)
-		printf("")
+	if (argc > 2 || argc <= 1)
+	{
+		printf(stderr, "USAGE: Monty file.\n")
+			exit(EXIT_FAILURE);
+	}
+
+	/* If, for any reason, it’s not possible to open the file,
+        /* print the error message Error: Can't open file <file>,
+        /* followed by a new line, and exit with the status EXIT_FAILURE */
+	/* insert fileReader function -> getline */
+
+	if (!funcPointer)
+	{
+		printf(stderr, "Error: Can't open file %s\n", funcPointer);
+	}
+
+	/* If the file contains an invalid instruction, print the error message
+	/* L<line_number>: unknown instruction <opcode>, followed by a new line,
+        /* and exit with the status EXIT_FAILURE */
+
+	if (funcPointer == EXIT_FAILURE)
+		exit(EXIT_FAILURE);
+
+	/* lineByLine function will read the data from file */
+	if (lineByLine(funcPointer) == EXIT_FAILURE)
+		exit (EXITFAILURE);
+
+	return(EXIT_SUCCESS);
+
+
 }
