bac2839e6af4828bb0695e668a754ba189e6007c
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-09-13 21:17:51 -0400

    Add: New main file

--- main.c
+++ main.c
@@ -1,49 +1,34 @@
 #include "monty.h"
 
-/**
- * main - The monty program in a nutshell
- * @argc: Number of arguments
- * @argv: The argument options
- *
- * Return: EXIT_SUCCESS or EXIT_FAILURE
- */
+node_t *node = NULL;
+
+void push_function(node_t **head, unsigned int line)
+{
+	printf("push function activated\n");
+}
+
+void pall_function(node_t **head, unsigned int line)
+{
+	printf("pall function activated\n");
+}
 
 int main(int argc, char **argv)
 {
 	char *path;
-	FILE *funcPointer;
-	unsigned int lineNum = 1;
+	FILE *fd;
+	unsigned int linenum = 1;
+	char *buffer = NULL;
+	size_t size = 0;
+	char *opcode = NULL, *command = NULL;
+	int i = 0;
 
-	/*If the user does not give any file or more than one argument to your
-	/* program, print the error message*/
-	if (argc > 2 || argc <= 1)
+	if (argc != 2)
 	{
-		printf(stderr, "USAGE: Monty file.\n")
-			exit(EXIT_FAILURE);
+		perror("Bad argument count");
 	}
 
-	/* If, for any reason, it’s not possible to open the file,
-        /* print the error message Error: Can't open file <file>,
-        /* followed by a new line, and exit with the status EXIT_FAILURE */
-	/* insert fileReader function -> getline */
+	/* argv[1] is the file name from command line */
+	path = argv[1];
 
-	if (!funcPointer)
-	{
-		printf(stderr, "Error: Can't open file %s\n", funcPointer);
-	}
-
-	/* If the file contains an invalid instruction, print the error message
-	/* L<line_number>: unknown instruction <opcode>, followed by a new line,
-        /* and exit with the status EXIT_FAILURE */
-
-	if (funcPointer == EXIT_FAILURE)
-		exit(EXIT_FAILURE);
-
-	/* lineByLine function will read the data from file */
-	if (lineByLine(funcPointer) == EXIT_FAILURE)
-		exit (EXITFAILURE);
-
-	return(EXIT_SUCCESS);
-
-
-}
+	if (!path)
+		perror("Error");
