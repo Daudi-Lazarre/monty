b9a1bc8a5bbdff3674122a52c3fe2b83cc3dbdb5
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-09-14 16:17:30 -0400

    added brackets

--- print_error.c
+++ print_error.c
@@ -23,29 +23,29 @@ void print_error(int n_error, unsigned int line_number)
 void print_err_a(int n_error, unsigned int line_number)
 {
 	if (n_error == ERR_PUSH)
-
+	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-
+	}
 	else if (n_error == ERR_PINT)
-
+	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-	
+	}
 	else if (n_error == ERR_POP)
-
+	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-
+	}
 	else if (n_error == ERR_SWAP)
-
+	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-
+	}
 	else if (n_error == ERR_ADD)
-
+	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-
+	}
 	else
+	{
 		return (ERROR_NOT_FOUND);
-
-
-
+	}
+	
 	return (ERROR_FOUND);
 }
